a043353b9a956dbc423cdb22ab881c789c4885f0
Bronwyn <bronellerbeck@gmail.com>
2023-09-19 21:22:12 +0200

    wip

--- contracts/RightfulShare.sol
+++ contracts/RightfulShare.sol
@@ -5,7 +5,7 @@ import "../node_modules/@openzeppelin/contracts/access/AccessControl.sol";
 
 contract RightfulShare is AccessControl {
 
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE"); // Should use default admin role? 0x00
+    bytes32 public constant ADMIN_ROLE = 0x00;
     bytes32 public constant GBH_ROLE = keccak256("GBH_ROLE");
     bytes32 public constant CONTRIBUTER_ROLE = keccak256("CONTRIBUTER_ROLE"); // seperate this functionality out?
 
@@ -13,9 +13,8 @@ contract RightfulShare is AccessControl {
 
     uint public latestId = 0; // id 0 is erserved as error indicator
 
-    function addMember(address _address) public returns(uint id) {
-        // only admin
-        // require doesn't exist already
+    function addMember(address _address) onlyRole(ADMIN_ROLE) public returns(uint id) {
+        require(ids[_address] == 0, "already member");
         latestId += 1;
         ids[_address] = latestId;
         return latestId;
